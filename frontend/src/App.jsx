ffa6d48b617ba4939265a9f3d66409bc4bbc7a22
Jatin Tyagi <tyagijatin6263@gmail.com>
2025-01-20 19:32:37 +0530

    create a not found page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 const Register = lazy(() => import('./pages/Register'));
 const Login = lazy(() => import('./pages/Login'));
 const Dashboard = lazy(() => import('./pages/Dashboard'));
+const NotFoundPage = lazy(() => import('./pages/NotFound'));
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
         <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='*' element={<NotFoundPage />} />
       </Routes>
     </HashRouter>
   )
